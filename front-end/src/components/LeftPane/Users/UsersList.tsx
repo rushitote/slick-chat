cf3463b6207d752ab64a2e7f0afe74a901c6b33c
Shashwat Khanna <shashwatkhanna312@gmail.com>
2021-10-13 23:10:21 +0530

    removed build folder and added newMessage event handler

--- front-end/src/components/LeftPane/Users/UsersList.tsx
+++ front-end/src/components/LeftPane/Users/UsersList.tsx
@@ -1,12 +1,29 @@
 import { useContext } from 'react'
 import messageContext from '../../../utils/messagesContext'
 import styles from './UsersList.module.css'
+import { Socket } from 'socket.io-client'
+import { useEffect } from 'react'
 export interface IUsersListProps {
   image: string
+  socket: Socket
+  groupId: string
 }
 
 export default function UsersList(props: IUsersListProps) {
+  useEffect(() => {
+    props.socket.emit(
+      'joinRoom',
+      JSON.stringify({
+        roomId: props.groupId,
+      })
+    )
+  })
   const ctx = useContext(messageContext)
+  useEffect(() => {
+    props.socket.on('newMessage', (data) => {
+      console.log(data)
+    })
+  }, [props.socket])
   return (
     <ul className={styles['user-list']}>
       <h1 className={styles['user-list-heading']}>Users</h1>
