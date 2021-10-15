d63b5e34cb06730c9978de09a7af0259c30ecf33
Shashwat Khanna <shashwatkhanna312@gmail.com>
2021-10-15 21:34:05 +0530

    changed heading size and text content

--- front-end/src/components/Login/About.tsx
+++ front-end/src/components/Login/About.tsx
@@ -6,12 +6,10 @@ export interface IAppProps {}
 export default function App(props: IAppProps) {
   return (
     <div className={styles['about']}>
-      <h1>React Chat App</h1>
+      <h1>Login</h1>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate
-        suscipit accusantium velit explicabo, iste neque facilis, sapiente
-        impedit placeat sint doloremque itaque molestias dolore. Asperiores
-        reiciendis modi ad facilis suscipit?
+        Logging in to your account ensures that your private chats are only for
+        you to see
       </p>
     </div>
   )
