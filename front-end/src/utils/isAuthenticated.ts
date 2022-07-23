bfdb867155d5518146a84cfcc9987f8070f7498c
Rushikesh Tote <rushi.tote@gmail.com>
2022-07-23 19:45:08 +0530

    wrap authenticated function with try catch

--- front-end/src/utils/isAuthenticated.ts
+++ front-end/src/utils/isAuthenticated.ts
@@ -1,15 +1,20 @@
 const isAuthenticated = async () => {
-  const response = await fetch(`${process.env.REACT_APP_HOST}/authenticated`, {
-    method: 'GET',
-    credentials: 'include', //includes the cookies
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    mode: 'cors',
-  })
-  const { authenticated } = await response.json()
-  // response is of format { authenticated: true|false}
-  return authenticated
+  try {
+    const response = await fetch(`${process.env.REACT_APP_HOST}/authenticated`, {
+      method: 'GET',
+      credentials: 'include', //includes the cookies
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      mode: 'cors',
+    })
+    const { authenticated } = await response.json()
+    // response is of format { authenticated: true|false}
+    return authenticated
+  } catch (e) {
+    console.log(e)
+    return false
+  }
 }
 
 export default isAuthenticated
